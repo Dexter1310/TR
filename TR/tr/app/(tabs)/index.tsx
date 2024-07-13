d5aa589e53c0637bad6ec0d04297fbfeba51a361
dexter <javier.orti@pistacero.net>
2024-07-13 10:04:49 +0200

    add negrok for --tunnel dev localhost

--- TR/tr/app/(tabs)/index.tsx
+++ TR/tr/app/(tabs)/index.tsx
@@ -1,70 +1,157 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Image, StyleSheet, Platform, View, Text, Button} from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import React, {SetStateAction, useState} from "react";
+import {ifElse} from "ansi-fragments";
+import is from "@sindresorhus/is";
+import set = is.set;
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+
+    const [display, setDisplay] = useState('');
+
+    const pressButton = (value: SetStateAction<string>) => {
+
+        if (display.length !== 4) {
+            setDisplay(display + value);
+        }
+
+        if (value == 'delete') {
+            let newdisplay = display.slice(0, -1);
+            setDisplay(newdisplay)
+        }
+        if(display==='0000'){
+            setDisplay('adm')
+        }
+
+
+    };
+
+    const Block = ({
+                       children,
+                       style,
+                       flex = 1,
+                       row, // <-- add this
+                       ...props
+                   }) => {
+        const blockStyle = StyleSheet.flatten([
+            flex !== undefined && { flex },
+            row && { flexDirection: 'row',padding:10}, // <-- add this
+            style,
+        ])
+
+        return (
+            <View style={blockStyle} {...props}>
+                {children}
+            </View>
+        )
+    }
+
+
+
+    // @ts-ignore
+    return (
+        <ParallaxScrollView
+            headerBackgroundColor={{light: '#A1CEDC', dark: '#1D3D47'}}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/favicon.png')}
+                    style={styles.reactLogo}
+                />
+            }>
+            <ThemedView style={styles.titleContainer}>
+                <ThemedText type="title">Welcome!</ThemedText>
+                <HelloWave/>
+            </ThemedView>
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Step 1: </ThemedText>
+                <ThemedText>
+                    Registra tu marcaje con tu código de usuario.
+
+                </ThemedText>
+            </ThemedView>
+
+            <View>
+                <View>
+                    <Text style={styles.fondoRojo}>{display}</Text>
+                </View>
+                <View>
+                    <Block row >
+                        <Block>
+                            <Button title="1" onPress={() => pressButton('1')}/>
+                        </Block>
+                        <Block>
+                            <Button title="2" onPress={() => pressButton('2')}/>
+                        </Block>
+                        <Block>
+                            <Button title="3" onPress={() => pressButton('3')}/>
+                        </Block>
+                    </Block>
+                    <Block row >
+                        <Block>
+                            <Button  title="4" onPress={() => pressButton('4')}/>
+                        </Block>
+                        <Block>
+                            <Button title="5" onPress={() => pressButton('5')}/>
+                        </Block>
+                        <Block>
+                            <Button title="6" onPress={() => pressButton('6')}/>
+                        </Block>
+                    </Block>
+                    <Block row>
+                        <Block>
+                            <Button title="7" onPress={() => pressButton('7')}/>
+                        </Block>
+                        <Block>
+                            <Button title="8" onPress={() => pressButton('8')}/>
+                        </Block>
+                        <Block>
+                            <Button title="9" onPress={() => pressButton('9')}/>
+                        </Block>
+                    </Block>
+
+                    <Block row>
+                        <Block>
+                            <Button title="0" onPress={() => pressButton('0')}/>
+                        </Block>
+                        <Block>
+                            <Button title="<-" onPress={() => pressButton('delete')}/>
+                        </Block>
+                    </Block>
+                </View>
+            </View>
+        </ParallaxScrollView>
+    );
 }
 
+
+
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    fondoRojo:{
+        backgroundColor:'yellow',
+        padding:10,
+        fontWeight:'bold'
+    },
+
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 178,
+        width: 290,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+
 });
